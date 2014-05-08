7311078c43090b4a83d8f69f169c990d0bfd9665
Sean Paul <seanpaul@chromium.org>
2014-05-08 11:45:52 -0400

    Revert "UPSTREAM: drm: add atomic fxns"
    This reverts commit f00d233ca55617368ff908e545777b72365a2bb3.
    
    There's a new version, it'll be better to roll this back and add
    the new one.
    
    BUG=chromium:336809
    TEST=Tested on snow & peppy
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Conflicts:
    	drivers/gpu/drm/Makefile
    	drivers/gpu/drm/cirrus/cirrus_drv.c
    	drivers/gpu/drm/exynos/exynos_drm_connector.c
    	drivers/gpu/drm/exynos/exynos_drm_drv.c
    	drivers/gpu/drm/exynos/exynos_drm_plane.c
    	drivers/gpu/drm/i915/i915_drv.c
    	drivers/gpu/drm/mgag200/mgag200_drv.c
    	drivers/gpu/drm/radeon/radeon_drv.c
    	drivers/gpu/drm/shmobile/shmob_drm_drv.c
    
    Change-Id: I631a1feb87025a7af8e8c39f413cd3852c83c61c

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -1628,8 +1628,7 @@ static int set_property(struct hdmi_context *hdata,
 }
 
 static int hdmi_connector_set_property(struct drm_connector *connector,
-		void *state, struct drm_property *property, uint64_t val,
-		void *blob_data)
+		struct drm_property *property, uint64_t val)
 {
 	struct hdmi_context *hdata = ctx_from_connector(connector);
 	int ret;
